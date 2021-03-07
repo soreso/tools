771069c4b08ad7a357c47d16bf3be2e38e4bd427
Soreso <55424278+soreso@users.noreply.github.com>
2021-03-07 18:26:37 +0900

    NULL合体演算子を使用中止

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 function twiSearch(){
-// (function(){
+// ;(function(){
   try {
     const url = location.href;
     // const url = 'https://twitter.com/soresoja/status/1367702843956731906';
     // const url = 'https://twitter.com/i/lists/931290338827874304';
-    const listIdArr = url.match(/(?<=\/lists\/)\d{15,20}/)　?? [null];
-    const userNameArr = url.match(/(?<=twitter\.com\/)\w*/) ?? [null];
-    // urlがlistの場合もuserNameArrは['i']になるので注意
+
+    const listIdArr = url.match(/(?<=\/lists\/)\d{15,20}/) || [null];
+    const userNameArr = url.match(/(?<=twitter\.com\/)\w*/) || [null];
+    // const listIdArr = url.match(/(?<=\/lists\/)\d{15,20}/)　?? [null];
+    // const userNameArr = url.match(/(?<=twitter\.com\/)\w*/) ?? [null];
+    // // urlがlistの場合もuserNameArrは['i']になるので注意
     let isList = false;
     let isUser = false;
     if(listIdArr[0] != null) {
@@ -14,11 +17,19 @@ function twiSearch(){
     } else if(userNameArr[0] != null && userNameArr[0].length >= 2) {
       isUser = true;
     } else {
-      throw new Error('【取得失敗】検索したいTwitterユーザーかリストのページで試して下さい');
+      // throw new Error('【取得失敗】検索したいTwitterユーザーかリストのページで試して下さい');
+      const isMoveDefault = window.confirm('【取得失敗】\nTwitterユーザーかリストのページで試して下さい。\nTwitterへ移動しますか？');
+      if(isMoveDefault !== false){
+        window.open('https://twitter.com/search?q=(filter%3Afollows)(-filter%3Areplies)&f=live', '_self');
+        throw new Error('');
+      } else {
+        // throw new Error('キャンセルしました');
+        throw new Error('');
+      }
     }
-
     // 検索ワードを入力してもらう
-    const searchWord1 = window.prompt('検索ワードを入力', '') ?? false;
+    const searchWord1 = window.prompt('検索ワードを入力', '') || false;
+    // const searchWord1 = window.prompt('検索ワードを入力', '') ?? false;
     if(searchWord1 === false){
       // throw new Error('キャンセルしました');
       throw new Error('');
@@ -42,7 +53,7 @@ function twiSearch(){
     if(e.message.length > 0){ window.alert(e.message); }
   }
 };
-// })()
+// }());
 
 // テキスト全選択
 function selectAll(target) {
@@ -55,4 +66,26 @@ function easyCopy() {
     copyTarget.select();
     document.execCommand("Copy");
     alert("全文コピーしました！");
+};
+
+// ;(function(){
+function hoge1() {
+  const a = null;
+  if(a===null){
+    alert('aはnullです');
+  } else {
+    alert('aはnull以外です');
+  }
+};
+// }());
+
+function hoge2() {
+  const b = false;
+  b === null ? alert('bはnullです') : alert('bはnull以外です');
+};
+
+function hoge3() {
+  const c = null;
+  const c2 = c ?? 'にゃー';
+  alert('c2は' + c2 + 'です');
 };
